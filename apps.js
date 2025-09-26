ada4dc3131ba6deef482bd776ee783e8eb5a7cf9
loylality <kawsar01994269452@gmail.com>
2025-09-26 09:34:15 -0700

    JavaScript first class code

---
+++ apps.js
@@ -0,0 +1,16 @@
+// function myFuction() {
+//     alert("Ami parbo")
+// }
+// var x, y, z;
+// x = 45;
+// y = 45;
+// total = x + y;
+// var minus = x - y;
+// var mul = x * y
+// document.write(x + "+" + y + "=" + total + "</br>" + x + "-" + y + "=" + minus + "</br>" + x + "x" + y + "=" + mul + "</br>");
+// var Sname = "Raz";
+// var Sroll = 02;
+// document.write("I am " + Sname + "." + "My roll is" + Sroll);
+var roll = 500;
+var name = " kawsar";
+console.log(roll + name)
